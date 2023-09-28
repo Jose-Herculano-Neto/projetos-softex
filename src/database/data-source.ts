e4d47444a3a0cae3f6bb189c1fc4ba90607a87a2
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-09-27 22:18:50 -0300

    configurações da api

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,8 +3,17 @@ import { DataSource } from "typeorm"
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
-    port: 3306,
+    port: 5432,
     username: "duuhnetoh",
     password: "123@duuh",
     database: "api-softex",
 });
+
+export const startDatabase = async () => {
+    try{
+        await AppDataSource.initialize();
+    }
+    catch(error){
+        console.log(error, 'Erro ao conectar com o banco de dados')
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import "reflect-metadata";
+import { startDatabase } from "./database/data-source";
 
-console.log("Hello, Wotld");
+
+
+const main = async() => {
+    try {
+        await startDatabase();
+        console.log("Banco de dados conectado com sucesso!");
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+main();
